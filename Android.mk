cff3fda2935db4d714b0288dc0f30ce28ddf3e7b
mydongistiny <jaysonedson@gmail.com>
2016-09-19 14:05:23 -0700

    Android.mk: Update version to 2.7.0
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,10 +1,10 @@
 # this is now the default FreeType build for Android
 #
 ifndef USE_FREETYPE
-USE_FREETYPE := 2.6.5
+USE_FREETYPE := 2.7.0
 endif
 
-ifeq ($(USE_FREETYPE),2.6.5)
+ifeq ($(USE_FREETYPE),2.7.0)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
